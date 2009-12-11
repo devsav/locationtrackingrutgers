c7598c94516660b163e9c6fc841ae0346e28f899
savio85 <savio85@64a5ae3c-c628-11de-8613-b9ccef3c78c6>
2009-12-11 16:51:22 +0000

    Last n queries

--- trunk/rails/location/app/controllers/map_controller.rb
+++ trunk/rails/location/app/controllers/map_controller.rb
@@ -30,6 +30,48 @@ class MapController < ApplicationController
       format.xml  { render :xml => @locations }
     end
     end
+
+    def lastN
+      # Create a new map object, also defining the div ("map") 
+      # where the map will be rendered in the view
+      @map = GMap.new("map")
+      # Use the larger pan/zoom control but disable the map type
+      # selector
+      @map.control_init(:large_map => true,:map_type => true)
+      # Center the map on specific coordinates and focus in fairly
+      # closely
+      #@map.center_zoom_init([41.023849,-80.682053], 10)
+
+      @locations = Location.find( :all, 
+                                  :conditions => [ "username=?",session[:user_id]],
+                                  :order => "time",
+                                  :limit => params[:id])
+                                  
+      init_location = Location.find(:first, :conditions => [ "username=?",session[:user_id]],
+                                  :order => "time",
+                                  :limit => 1)
+      #@map.addControl(new GMapTypeControl());
+
+      if init_location
+        @map.center_zoom_init([init_location.latitude,init_location.longitude], 10)  
+      else
+         @map.center_zoom_init([40.468176,-74.445227], 10)  
+      end
+      print @locations
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @locations }
+      end
+      end
+
+
+
+
+
+
+
+
  end
 
 
